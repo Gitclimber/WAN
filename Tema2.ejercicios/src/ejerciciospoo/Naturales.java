97da3fd831607986532b1c9024bfd3441a793090
Gitclimber <57266060+Gitclimber@users.noreply.github.com>
2019-11-18 12:44:41 +0100

    Add files via upload

--- Tema2.ejercicios/src/ejerciciospoo/Naturales.java
+++ Tema2.ejercicios/src/ejerciciospoo/Naturales.java
@@ -3,7 +3,18 @@ package ejerciciospoo;
 
 
 public class Naturales {
-    public static void main(String[] args) {
-        
+    
+    
+    public static void mais(String[] args) {
+        for(int i=0;i<100; i++) {
+            System.out.println(i);
+        }
+    }
+
+
+public static void menos(String[] args) {
+        for(int i=100;i>0; i--) {
+            System.out.println(i);
+        }
     }
 }
