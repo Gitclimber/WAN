6d65ba6f48b762c6488ee5ff7763245074091918
Gitclimber <57266060+Gitclimber@users.noreply.github.com>
2019-11-18 12:42:23 +0100

    Add files via upload

---
+++ Tema2.ejercicios/src/ejerciciospoo/Naturales.java
@@ -0,0 +1,9 @@
+
+package ejerciciospoo;
+
+
+public class Naturales {
+    public static void main(String[] args) {
+        
+    }
+}
--- Tema2.ejercicios/src/ejerciciospoo/Rectangulo.java
+++ Tema2.ejercicios/src/ejerciciospoo/Rectangulo.java
@@ -3,31 +3,31 @@ package ejerciciospoo;
 
 
 public class Rectangulo {
-    double x;
-    double y;
-     public  double definirladox(double ladox) {
-         this.x = ladox;
-         
-        return ladox;
-        
-        
+    //ATRIBUTOS
+    private int  ladox;
+    private int ladoy;
+    
+    //MÉTODOS
+     public  void definirladox(int x) {
+         this.ladox = x;
+         }
+     
+      public void definirladoy(int y) {
+         this.ladoy = y;
+     }
+     public void definirlados(int x, int y) {
+         this.ladox = x;
+         this.ladoy = y;
      }
      
-      public  double definirladoy(double ladoy) {
-         this.y = ladoy;
-         
-        return ladoy;
-        
-        
-     }
-     public double calculararea() {
-         double area = this.x*this.y;
-         return area;
-     }
-     
-   public double calcularperimetro() {
-       double perimetro =2*this.x + 2*this.y;
-       return perimetro;
+   public int calculaperimetro() {
+       
+       return (2*ladox + 2*ladoy);
    }
+   public int calculaarea() {
+       
+       return (ladox*ladoy);
+   }
+   
    }
 
